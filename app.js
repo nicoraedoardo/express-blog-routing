37197a857abc6ff98b6f6fa87fddce2215f123e5
nicoraedoardo <edoardo.nicora26@gmail.com>
2026-03-11 17:34:55 +0100

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// importiamo il router
 const postsRouter = require("./routers/posts");
 
 // registriamo il router con /posts
